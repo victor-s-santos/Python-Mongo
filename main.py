a91590a76bd81a9fae5dc14d9a5c95dc0843d57a
Victor <victor.santos.cd@gmail.com>
2022-10-10 09:03:40 -0300

    feat(python_main): Create some of basics methods to python-mongo class

--- main.py
+++ main.py
@@ -4,14 +4,17 @@ class PymongoConnection:
     def __init__(self) -> None:
         pass
 
-    def create_connection(self):
+    def my_client(self):
         """Create the mongodb connection."""
         return pymongo.MongoClient("mongodb://localhost:27017/")
 
 
     def create_db(self):
         """Create the first_database database if is does not exist."""
-        return self.create_connection(["first_database"])
+        return self.my_client()["first_database"]
 
-
+    
+    def check_database_names(self):
+        """Return a list of the name of databases."""
+        return self.my_client().list_database_names()
 
